f7d4408ee52fec1964ac56bd82fb877657239c12
rdvapk <rdvapk@gmail.com>
2024-08-19 09:14:55 +0000

    belajargetx

---
+++ lib/app/modules/counter/views/counter_view.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/counter_controller.dart';
+
+class CounterView extends GetView<CounterController> {
+  const CounterView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // memanggil counter controller
+    CounterController c = Get.put(CounterController());
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CounterView'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Obx(() => Text('di klik : ${c.bilangan}')),
+          ElevatedButton(
+              onPressed: () {
+                c.tambahsatu();
+              },
+              child: Icon(Icons.add)),
+
+          ElevatedButton(
+              onPressed: () {
+                c.kurangSatu();
+              },
+              child: Icon(Icons.remove),)
+        ],
+        ),
+      ),
+    );
+  }
+}
