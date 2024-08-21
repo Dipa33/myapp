3b9c16fdc2bf1eeef1e313328c746e6849d6d16d
rdvapk <rdvapk@gmail.com>
2024-08-21 10:00:00 +0000

    login

---
+++ lib/app/modules/profile/controllers/profile_controller.dart
@@ -0,0 +1,29 @@
+import 'package:get/get.dart';
+
+class ProfileController extends GetxController {
+  //TODO: Implement ProfileController
+
+  final count = 0.obs;
+
+  get isLoading => null;
+
+  get userProfile => null;
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  void increment() => count.value++;
+
+  logout() {}
+}
